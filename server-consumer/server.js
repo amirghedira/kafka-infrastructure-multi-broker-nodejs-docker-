49cafdc427e39bf25cc4fb58df373a0d7da3d11a
amirghedira <amirghedira06@gmail.com>
2021-12-05 16:01:54 +0100

    first commit

---
+++ server-consumer/server.js
@@ -0,0 +1,28 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094']
+})
+
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const runConsumer = async () => {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic-2', fromBeginning: true })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+
+runConsumer()
+    .catch(err => {
+        console.log(err)
+    })
---
+++ server-producer/server.js
@@ -0,0 +1,41 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094']
+})
+const producer = kafka.producer()
+producer.connect()
+    .then(res => {
+        console.log('producer server successfully started')
+    })
+    .catch(err => {
+        console.log('producer server failed')
+    })
+
+
+const queueMessage = async (order) => {
+    return await producer.send({
+        topic: 'test-topic-2',
+        messages: [
+            { value: JSON.stringify(order) },
+        ],
+    })
+
+}
+
+setInterval(() => {
+    const newOrder = {
+        _id: "34343",
+        clientId: "2323",
+        price: 120
+    }
+    queueMessage(newOrder)
+        .then(() => {
+            console.log('sended message:', newOrder)
+
+        })
+        .catch(err => {
+            console.log('failed message:', newOrder)
+        })
+}, 5000)
